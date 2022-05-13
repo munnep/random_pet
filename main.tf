2387ddff206f45df21a0aa9b73f36adece946877
munnep <patrick.munne@hashicorp.com>
2022-05-13 09:18:23 +0200

    test for patrick

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@ resource "random_pet" "pet1" {}
 
 resource "random_pet" "pet2" {}
 
-resource "random_pet" "pet3" {}
-
-resource "random_pet" "patrick" {}
-
-resource "random_pet" "andrii" {}
-
-resource "random_pet" "anton" {}
-
-resource "random_pet" "lotte" {}
-
-resource "random_pet" "sysan" {}
 
 output "pet1" {
   value = random_pet.pet1.id
